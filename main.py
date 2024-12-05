3a5f722a6ec4a0e6a6584fb124029031cdb663e5
silas <i@reao.io>
2024-12-05 18:01:36 +0800

    [fix] bind addr to 0.0.0.0

--- main.py
+++ main.py
@@ -101,4 +101,4 @@ async def root():
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=config.get('port', 5100))
+    uvicorn.run(app, host="0.0.0.0", port=config.get('port', 5100))
